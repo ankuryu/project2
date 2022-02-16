b5a0f2558ac84b7c3ccc9aca110c5131aa3d02d1
ankuryu <pmco89@gmail.com>
2022-02-16 07:27:29 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const e = require('express');
+const express = require('express');
+const path = require('path');
+
+
+const app = express();
+app.set('view engine',"ejs")
+app.set("views",path.join(__dirname,"views");
+app.use(express.static(path.join(__dirname,"public")));
+
+app.listen(3000,()=>{
+    console.log("Server started on Port 3000")
+});
+
+
+app.get('/',(req,res)=> {
+    res.status(200).send("<p>Hello World</p>")
+});
