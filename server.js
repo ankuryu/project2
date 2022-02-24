2816924e99ab0588fe403b4f18b3e389ac08d99e
Sunil Gandhi <34267874+ankuryu@users.noreply.github.com>
2022-02-24 12:11:17 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-const e = require('express');
-const express = require('express');
-const path = require('path');
-
+//const e = require('express');
+const express = require("express");
+const path = require("path");
 
 const app = express();
-app.set('view engine',"ejs")
-app.set("views",path.join(__dirname,"views");
-app.use(express.static(path.join(__dirname,"public")));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000,()=>{
-    console.log("Server started on Port 3000")
+app.listen(3000, () => {
+  console.log("Server started on Port 3000");
 });
 
-
-app.get('/',(req,res)=> {
-    res.status(200).send("<p>Hello World</p>")
+app.get("/", (req, res) => {
+  res.status(200).send("<p>Hello World</p>");
 });
