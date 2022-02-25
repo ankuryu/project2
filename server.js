cf2d917b94aec5ea191b7a134d6b3e2e929b331b
Sunil Gandhi <34267874+ankuryu@users.noreply.github.com>
2022-02-25 15:44:23 +0530

    Complete CRUD oper Full CRUD operation working using Express,Sqlite and EJS

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(express.urlencoded({ extended: false })); // <--- middleware configuration
+
 app.listen(3000, () => {
   console.log("Server started on Port 3000");
 });
@@ -45,6 +47,66 @@ app.get("/books", (req, res) => {
     if (err) {
       return console.error(err.message);
     }
-    res.render("books", { model: rows });
+    res.render("books2", { model: rows });
+  });
+});
+
+// GET /edit/5
+app.get("/edit/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = "SELECT * FROM Books WHERE Book_ID = ?";
+  dbo.dbcon.get(sql, id, (err, row) => {
+    // if (err) ...
+    res.render("edit", { model: row });
+  });
+});
+
+// POST /edit/5
+app.post("/edit/:id", (req, res) => {
+  const id = req.params.id;
+  const book = [req.body.Title, req.body.Author, req.body.Comments, id];
+  const sql =
+    "UPDATE Books SET Title = ?, Author = ?, Comments = ? WHERE (Book_ID = ?)";
+  dbo.dbcon.run(sql, book, (err) => {
+    // if (err) ...
+    res.redirect("/books");
+  });
+});
+
+// GET /create
+app.get("/create", (req, res) => {
+  const book = {
+    Author: "Ankuryu Zekko"
+  };
+  res.render("create", { model: book });
+});
+
+// POST /create
+app.post("/create", (req, res) => {
+  const sql = "INSERT INTO Books (Title, Author, Comments) VALUES (?, ?, ?)";
+  const book = [req.body.Title, req.body.Author, req.body.Comments];
+  dbo.dbcon.run(sql, book, (err) => {
+    // if (err) ...
+    res.redirect("/books");
+  });
+});
+
+// GET /delete/5
+app.get("/delete/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = "SELECT * FROM Books WHERE Book_ID = ?";
+  dbo.dbcon.get(sql, id, (err, row) => {
+    // if (err) ...
+    res.render("delete", { model: row });
+  });
+});
+
+// POST /delete/5
+app.post("/delete/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = "DELETE FROM Books WHERE Book_ID = ?";
+  dbo.dbcon.run(sql, id, (err) => {
+    // if (err) ...
+    res.redirect("/books");
   });
 });
